1c5389c2cfc1c3fdb8b3b49025bf75d903d63b02
t0xicwise <46230448+t0xicwise@users.noreply.github.com>
2019-01-02 12:15:09 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,20 +7,7 @@ const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");//youtube api key
 const queue = new Map();//the queue map
 const token = process.env.BOT_TOKEN//your bot token
-const prefix = '#'//your prefix
-
-
-//All copytights for Eyad & (old) Codes
-
-/*
-Packages
-npm install discord.js
-npm install ytdl-core
-npm install get-youtube-id
-npm install youtube-info
-npm install simple-youtube-api
-npm install queue
-*/
+const prefix = '1'//your prefix
 
 client.on('ready', () => {//the ready msgs
     console.log(`Logged in as ${client.user.tag}!`);
